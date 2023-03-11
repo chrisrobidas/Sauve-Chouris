291cee7590925b5c81203b829dbb5f7d51fe4209
Mika Dubé <domino882010@hotmail.com>
2023-03-10 23:23:51 -0500

    Added basic echolocation script and Empty circle sprite.

--- Assets/Scripts/Echolocation.cs
+++ Assets/Scripts/Echolocation.cs
@@ -5,25 +5,55 @@ using UnityEngine;
 
 public class Echolocation : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 1f;
+    [SerializeField]
+    float maxRange = 5f;
+    [SerializeField]
+    SpriteRenderer echoSprite;
+
+    float range = 0f;
+    bool echoActive = false;
+    List<Collider2D> collidersHit = new List<Collider2D> ();
     private void Start()
     {
-        
+        echoSprite.enabled = false;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (range > maxRange)
         {
-            EchoLocalize();
+            echoActive = false;
+            range = 0f;
+            collidersHit.Clear();
+            echoSprite.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !echoActive)
+        {
+            echoActive = true;
+        }
+
+        if (echoActive)
+        {
+            range += speed * Time.deltaTime;
+            echoSprite.enabled = true;
+            echoSprite.transform.localScale = new Vector3(range, range);
+            EchoLocalize(range);
         }
     }
 
-    public void EchoLocalize()
+    public void EchoLocalize(float radius)
     {
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, 50, new Vector2(0, 0));
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, radius * 2f, new Vector2(0, 0));
         foreach (RaycastHit2D hit in hits)
         {
-            Debug.Log("Hit object at x: " + hit.transform.position.x + ", y: " + hit.transform.position.y);
+            if (!collidersHit.Contains(hit.collider))
+            {
+                collidersHit.Add(hit.collider);
+                Debug.Log("Hit object at x: " + hit.transform.position.x + ", y: " + hit.transform.position.y);
+            }
         }
     }
 }
