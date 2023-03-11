e3a5b2fdf5a581fc23e74183206d20be77e0d040
Jacty <saej3101@usherbrooke.ca>
2023-03-11 12:33:06 -0500

    Basic chase

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
@@ -18,25 +19,58 @@ public class Enemy : MonoBehaviour
 
     private SpriteRenderer _spriteRenderer;
     
+    [FormerlySerializedAs("_detectionLayer")] [SerializeField] private LayerMask detectionLayer;
+    [SerializeField] private float _radiusRange = 2f;
+    [SerializeField] private string playerTag = "Player";
+
+    private Vector3 _target;
+    private int _currentWaypointIndex = 0;
+    private bool _isChasing = false;
+    private Transform _playerTarget;
+    
+
+
     void Start()
     {
         _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         waypoints = GameObject.FindGameObjectsWithTag(waypointsTag).Select(x => x.transform).ToList();
         _currentWaypointIndex = GetRandWaypointIndex();
         
+        _target = new Vector3(0, 0, 0);
+        _playerTarget = GameObject.FindGameObjectWithTag(playerTag).transform;
+
     }
 
     void Update()
     {
-        if (waypoints.Count > 0)
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, _radiusRange, detectionLayer.value);
+        if (hit)
         {
-            Vector3 targetPosition = waypoints[_currentWaypointIndex].position;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            float TOLERANCE = 10e-6f;
-            if (Math.Abs(transform.position.x - targetPosition.x) < TOLERANCE && Math.Abs(transform.position.y - targetPosition.y) < TOLERANCE)
-            {
-                _currentWaypointIndex = GetRandWaypointIndex();
-            }
+            Debug.Log("hit");
+            Chase();
+        }
+        
+        if (_isChasing)
+        {
+            _target = _playerTarget.position;
+        }
+        else
+        {
+            CheckWaypoints();
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, _target, 1f * Time.deltaTime);
+    }
+
+    void CheckWaypoints()
+    {
+        if (_waypoints.Count <= 0) return;
+        
+        _target = _waypoints[_currentWaypointIndex].position;
+        const float TOLERANCE = 10e-6f;
+        if (Math.Abs(transform.position.x - _target.x) < TOLERANCE && Math.Abs(transform.position.y - _target.y) < TOLERANCE)
+        {
+            _currentWaypointIndex = GetRandWaypointIndex();
         }
     }
 
@@ -62,6 +96,19 @@ public class Enemy : MonoBehaviour
         }
 
         return _currentWaypointIndex;
+    }
+
+    void Chase()
+    {
+        _isChasing = true;
+    }
+    
+    
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radiusRange);
     }
     
     
