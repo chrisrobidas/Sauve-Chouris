ef727d462b6fa673d5d1d5ee2bf04f595e631ef9
Jacty <saej3101@usherbrooke.ca>
2023-03-11 23:26:20 -0500

    Add game over

--- Assets/Scripts/Player/PlayerGameOver.cs
+++ Assets/Scripts/Player/PlayerGameOver.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerGameOver : MonoBehaviour
 {
-    // public void OnTriggerEnter()
-    // {
-    //     FindObjectOfType()
-    // }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
 }
+    
