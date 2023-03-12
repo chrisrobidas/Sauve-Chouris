8668e6f32b97ed461f2c90283f982fc52208cd59
Jacty <saej3101@usherbrooke.ca>
2023-03-11 21:47:00 -0500

    WIP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _gameIsEnded = true;
+
+    public void EndGame()
+    {
+        if (!_gameIsEnded)
+        {
+            _gameIsEnded = true;
+            Restart();
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerGameOver.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerGameOver : MonoBehaviour
+{
+    // public void OnTriggerEnter()
+    // {
+    //     FindObjectOfType()
+    // }
+}
